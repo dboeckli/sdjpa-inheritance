2f7d925054413dff84ff360f67e87f9c23121d05
adm-boeckli <dboeckli@gmail.com>
2025-04-11 17:04:58 +0200

    add mapped supperclass

---
+++ src/main/java/ch/dboeckli/guru/jpa/sdjpa/inheritance/domain/BaseEntity.java
@@ -0,0 +1,43 @@
+package ch.dboeckli.guru.jpa.sdjpa.inheritance.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+// Equals und Hashcode wurden mit JPA Buddy erstellt gemäss:
+// https://jpa-buddy.com/blog/hopefully-the-final-article-about-equals-and-hashcode-for-jpa-entities-with-db-generated-ids/
+public abstract class BaseEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        BaseEntity that = (BaseEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+}
---
+++ src/main/java/ch/dboeckli/guru/jpa/sdjpa/inheritance/domain/OrderHeader.java
@@ -0,0 +1,15 @@
+package ch.dboeckli.guru.jpa.sdjpa.inheritance.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class OrderHeader extends BaseEntity {
+
+    private String customerName;
+}
